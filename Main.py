ebe8c42b566a78faebd1cdb6eed02a6211790bad
jsxhhyf <jsxhhyf@gmail.com>
2016-05-25 20:38:29 +0800

    1. add network communication 2. basic function complete

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import random, ipaddress, urllib
+import random, ipaddress, urllib.parse, urllib.request, json, time
 
 # IPv6 addressing and CABA addressing
 
@@ -11,14 +11,23 @@ RLOCs = ['2001:1726:c363:d36e:2ab7:630a:d9d8:1',
          '2001:1726:c363:d36e:2ab7:630a:d9d8:7',
          '2001:1726:c363:d36e:2ab7:630a:d9d8:8']
 
-for i in range(0, 100):
+for i in range(0, 50):
     RES = '00000000'
     ASN = '00000000' + str(bin(random.randint(0, 16777216)))[2:]
     SNID = str(bin(random.randint(0, 16777216)))[2:]
     IID = str(bin(random.randint(0, 16777216)))[2:] + str(bin(random.randint(0, 16777216)))[2:] + str(bin(random.randint(0, 16777216)))[2:]
-    EID = RES + ASN + SNID + IID
-# b = int('00000101111111111111111100000', 2)
-    print()
+    EID_b = RES + ASN + SNID + IID
 
     RLOC = RLOCs[random.randint(0, 7)]
-    print(ipaddress.IPv6Address(int(EID, 2)), RLOC)
+    EID = ipaddress.IPv6Address(int(EID_b, 2))
+
+    url = 'http://10.114.2.33:8080/wm/mappingtablemanager/json'
+    value = "{\"dst_eid\":\"" + str(EID) + "\", \"dst_rloc\":\"" + RLOC + "\"}"
+    # value = "\"dst_eid\":\"" + str(EID) + "\", \"dst_rloc\":\"" + RLOC + "\""
+    print(value)
+    # data = urllib.parse.urlencode(value)
+    binary_data = value.encode('utf8')
+    req = urllib.request.Request(url, binary_data)
+    response = urllib.request.urlopen(req)
+    print(response.read())
+    time.sleep(1)
